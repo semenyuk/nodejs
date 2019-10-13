a749671a712918c1b7e1806b0f952089216f2e41
Aleksandr <aleksandr@Aleksandrs-MacBook-Pro.local>
2019-10-13 14:36:54 +0300

    added routes, logger, error handler

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const router = require('./router');
+const swaggerDocument = require('../swagger.json');
+
+const app = express();
+const port = 3000;
+
+app.use('/api/v1/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use('/', router);
+
+app.use((req, res) => {
+    res.status(404).send('Route ' + req.url + ' Not found.');
+});
+
+app.use((err, req, res, next) => {
+    console.error('\x1b[31m', err.stack);
+    res.status(500).send(process.env.NODE_ENV === 'development' ? err.stack : err.message);
+});
+
+app.listen(port, () => console.log(`nodejs-course app listening on port ${port}!`));
---
+++ src/router.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => res.send('Hello World!'));
+
+router.get('/api/v1/readme', (req, res, next) => {
+    fs.readFile('./README1.md', (err, data) => {
+        if (err) {
+            return next(err);
+        }
+
+        res.send(data.toString())
+    });
+});
+
+module.exports = router;
